63d3f8f21eac60f58b48772daf66f428ea30b019
XT Joe <31270922+FabregasZhou@users.noreply.github.com>
2020-04-28 20:01:46 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,19 +15,19 @@ hello_meg = "我们已经在一起" + str(together_days) + "天了哦\n"
 hour_now = datetime.now().hour
 
 if hour_now < 6 or hour_now >= 18:
-    hello_head = "豆豆晚安安安安安安安安~"
+    hello_head = "晚安安安安安安安安~"
     if hour_now < 2:
         hello_meg = hello_meg + "快准备睡觉觉！ 爱你呀！"
     else:
         hello_meg = hello_meg + "晚上也要加油哦~  爱你哦~"
 elif hour_now >= 6 and hour_now < 11:
-    hello_head = "豆豆早安安安安安！"
+    hello_head = "早安安安安安！"
     hello_meg = hello_meg + "又是元气满满的一早哦~  爱你~"
 elif hour_now >= 11 and hour_now < 1:
-    hello_head = "豆豆午安安安安安安！"
+    hello_head = "午安安安安安安！"
     hello_meg = hello_meg + "中午有没有吃饱哦"
 else:
-    hello_head = "豆豆下午安安安安安！"
+    hello_head = "下午安安安安安！"
     hello_meg = hello_meg + "下午也要元气满满哦~  爱你！！"
 
 hello_toaster = ToastNotifier()
@@ -55,7 +55,7 @@ wea_tomorrow = weather_list[1].find('p', class_='wea').string
 tem_tomorrow = weather_list[1].find('p', class_='tem').find('i').string
 wind_tomorrow = weather_list[1].find('p', class_='win').find('i').string
 
-weather_head = "瓜瓜天气预报"
+weather_head = "天气预报"
 weather_meg = "今天：" + wea_today + " " + tem_today + " " + wind_today
 if int(tem_today[:-1]) < 5:
     weather_meg = weather_meg + " 注意保暖哦~\n"
